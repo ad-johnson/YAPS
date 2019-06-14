f937fa2879ed3c35718c7a72002a33b797555448
Andrew Johnson <ad-johnson@users.noreply.github.com>
2019-06-14 15:51:06 +0100

    Add files via upload

---
+++ YapsGraphical/YapsGraphicalConst.h
@@ -0,0 +1,65 @@
+// File generated 14/06/2019 13:15:01
+// Warning! This is a generated file, any manual changes will be
+// lost during the next generation.
+
+#if (ARDUINO >= 100)
+	#include "Arduino.h" // for Arduino 1.0
+#else
+	#include "WProgram.h" // for Arduino 23
+#endif
+
+// object indexes into ImageControl
+#define iMeter1                 0 // offset 0x0
+#define iThermometer1           1 // offset 0x45C00
+#define iStatictext2            2 // offset 0x159400
+#define iStatictext1            3 // offset 0x159800
+#define iStatictext3            4 // offset 0x159E00
+#define iLeddigits1             5 // offset 0x15A400
+#define iiLeddigits1            6 // offset 0x15CC00
+#define iLeddigits2             7 // offset 0x161C00
+#define iiLeddigits2            8 // offset 0x163C00
+#define iStatictext4            9 // offset 0x168C00
+#define iCustomdigits1         10 // offset 0x169400
+#define iiCustomdigits1        11 // offset 0x169600
+#define iStatictext5           12 // offset 0x16A000
+#define iCustomdigits2         13 // offset 0x16A200
+#define iiCustomdigits2        14 // offset 0x16A600
+#define iStatictext6           15 // offset 0x16B000
+#define iStatictext7           16 // offset 0x16B400
+#define iCustomdigits3         17 // offset 0x16BA00
+#define iiCustomdigits3        18 // offset 0x16BC00
+#define iStatictext8           19 // offset 0x16C600
+#define iCustomdigits4         20 // offset 0x16C800
+#define iiCustomdigits4        21 // offset 0x16CC00
+#define iStatictext9           22 // offset 0x16D600
+#define iStatictext10          23 // offset 0x16D800
+#define iStatictext11          24 // offset 0x16DE00
+#define iStatictext12          25 // offset 0x16E800
+#define iStatictext13          26 // offset 0x16F000
+#define iStatictext14          27 // offset 0x16FE00
+#define iCustomdigits5         28 // offset 0x170C00
+#define iiCustomdigits5        29 // offset 0x171000
+#define iCustomdigits6         30 // offset 0x171A00
+#define iiCustomdigits6        31 // offset 0x171E00
+#define iCustomdigits7         32 // offset 0x172800
+#define iiCustomdigits7        33 // offset 0x172C00
+#define iStatictext15          34 // offset 0x173600
+#define iStatictext16          35 // offset 0x173800
+#define iStatictext17          36 // offset 0x173A00
+#define iCustomdigits8         37 // offset 0x173C00
+#define iiCustomdigits8        38 // offset 0x173E00
+#define iCustomdigits9         39 // offset 0x174800
+#define iiCustomdigits9        40 // offset 0x174A00
+#define iCustomdigits10        41 // offset 0x175400
+#define iiCustomdigits10       42 // offset 0x175600
+#define iCustomdigits11        43 // offset 0x176000
+#define iiCustomdigits11       44 // offset 0x176200
+#define iCustomdigits12        45 // offset 0x176C00
+#define iiCustomdigits12       46 // offset 0x177000
+#define iStatictext18          47 // offset 0x177A00
+#define iCustomdigits13        48 // offset 0x177C00
+#define iiCustomdigits13       49 // offset 0x178000
+#define iStatictext19          50 // offset 0x178A00
+#define iCustomdigits14        51 // offset 0x178C00
+#define iiCustomdigits14       52 // offset 0x178E00
+
